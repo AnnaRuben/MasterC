7c4ea645b965a8ffed50ca6b1a5be07d2a2a702a
AnnaRuben <shaked12072014@gmail.com>
2026-07-21 06:02:43 +0300

    RequirementsHomework

--- Conditionals/HomeWorkConditionalStatements.cs
+++ Conditionals/HomeWorkConditionalStatements.cs
@@ -10,18 +10,18 @@
 //}
 
 
-Console.Write("What is your first name: ");
-string? firstName = Console.ReadLine();
+//Console.Write("What is your first name: ");
+//string? firstName = Console.ReadLine();
 
-Console.Write("What is your last name: ");
-string? lastName = Console.ReadLine();
+//Console.Write("What is your last name: ");
+//string? lastName = Console.ReadLine();
 
-if (firstName.ToLower() == "tim" &&
-    lastName.ToLower() == "corey")
-{
-    Console.WriteLine("Hello Professor Corey");
-}
-else
-{
-    Console.WriteLine("Hello Student.");
-}
+//if (firstName.ToLower() == "tim" &&
+//    lastName.ToLower() == "corey")
+//{
+//    Console.WriteLine("Hello Professor Corey");
+//}
+//else
+//{
+//    Console.WriteLine("Hello Student.");
+//}
---
+++ Conditionals/RequirementsHomework.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Conditionals
+//{
+//    internal class RequirementsHomework
+//    {
+//    }
+//}
+
+
+Console.Write("Hello, what is your first name: ");
+string? firstName = Console.ReadLine();
+
+Console.Write("How old are you: ");
+string? ageText = Console.ReadLine();
+int age = int.Parse(ageText);
+
+if (firstName.ToLower() == "bob" ||
+    firstName.ToLower() == "sue")
+{
+    Console.WriteLine($"Hello Professor {firstName}");
+}
+
+if (age < 21)
+{
+    Console.WriteLine($"I recommend you wait {age} years.");
+}
+else
+{
+    Console.WriteLine("Welcome to the course.");
+}
