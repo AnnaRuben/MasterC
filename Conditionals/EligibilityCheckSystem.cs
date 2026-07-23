b4a666582869dc3ca7ab5cecd3d599272403c2d2
AnnaRuben <shaked12072014@gmail.com>
2026-07-23 05:58:30 +0300

    Eligibility Check System

---
+++ Conditionals/EligibilityCheckSystem.cs
@@ -0,0 +1,166 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Conditionals
+//{
+//    internal class EligibilityCheckSystem
+//    {
+//    }
+//}
+using System.Globalization;
+
+Console.Write("Hello User! ,What is your first name: ");
+string? firstName = Console.ReadLine();
+
+Console.Write($"Hello {firstName}! ,What is your last name: ");
+string? lastName = Console.ReadLine();
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+string? filePath = @"C:\Students\Grants";
+Console.WriteLine($"Save file to {filePath} ,based on your name: {firstName} {lastName}.");
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Console.Write($"{firstName} ,What's your age: ");
+string? ageText = Console.ReadLine();
+
+bool isValidInt = int.TryParse(ageText, out int age);
+
+Console.WriteLine($"This is valid: {isValidInt}. The number was {age}.");
+
+
+if (!isValidInt)
+{
+    age = 0;
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+DateOnly birthday = DateOnly.Parse("23/06/1983");
+
+Console.WriteLine(birthday.ToString("dd, MMMM, yyyy", CultureInfo.InvariantCulture));
+Console.WriteLine($"Birthday full format: {birthday}");
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Console.Write($"Hey {firstName}, What is your grade point average: ");
+double? gradePointAverage = Console.ReadLine();
+Console.WriteLine(gradePointAverage = 85.5);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Console.Write($"Hey {firstName}, What is your monthly income: ");
+decimal? monthlyIncome = Console.ReadLine();
+Console.WriteLine(monthlyIncome - 6000);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+bool? hasMilitaryService = null;
+bool? hasMilitaryService = true;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if (firstName.ToLower() == "bob" && 
+    firstName.ToLower() == "sue")
+{
+    Console.WriteLine($" Hello Professor {firstName}");
+}
+else if (firstName.ToLower() == "bob" ||
+         firstName.ToLower() == "sue")
+{
+    Console.WriteLine("You have a great part in your name.");
+}
+else
+{
+    Console.WriteLine("Hello Student");
+}
+
+if (age >= 40 && age < 50 ||
+    age >= 70 && age < 80)
+{
+    Console.WriteLine("You are in your 40's");
+}
+
+
+switch (age)
+{
+    case >= 0 and < 17:
+        Console.WriteLine("You are a minor.");
+        break;
+    case >= 18 and < 65:
+        Console.WriteLine("You are a fit for checkup.");
+        break;
+    case > 65:
+        Console.WriteLine("Wish you a Happy Retirement.");
+        break;
+    default:
+        Console.WriteLine("Undefined.");
+        break;
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+bool? isComplete = true;
+
+if (gradePointAverage == 85.5 &&
+    monthlyIncome < 6000)
+{
+    Console.WriteLine("Congrats! Registration process was completed successfully");
+}
+else
+{
+    Console.WriteLine("Sorry, try next year.");
+}
+
+
+switch (gradePointAverage)
+{
+    case >= 0 and < 85.5:
+        Console.WriteLine("Congrats! You are a candidate for Eligibility Program.");
+        break;
+    case >= 0 and < 6000:
+        Console.WriteLine("Congrats! You are a candidate for Eligibility Program.");
+        break;
+    default:
+
+        break;
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+TimeOnly  
+Console.WriteLine($"Hello {firstName} {lastName}!,Your file was saved successfully to {filePath} ,based on your name: {firstName} {lastName}. we saved your Birthday: {birthday}.");
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Conditionals/Registration&Check_in_Desk.cs
+++ Conditionals/Registration&Check_in_Desk.cs
@@ -13,72 +13,72 @@
 
 
 
-using System.Globalization;
+//using System.Globalization;
 
-Console.Write("Hello user! What is your first name? ");
-string? firstName = Console.ReadLine();
+//Console.Write("Hello user! What is your first name? ");
+//string? firstName = Console.ReadLine();
 
-Console.Write($"Hello {firstName}! What is your last name? ");
-string? lastName = Console.ReadLine();
+//Console.Write($"Hello {firstName}! What is your last name? ");
+//string? lastName = Console.ReadLine();
 
-Console.Write($"Hello {firstName} {lastName}! How old are you? ");
-string? textAge = Console.ReadLine();
-bool isValidAge = int.TryParse(textAge, out int age);
+//Console.Write($"Hello {firstName} {lastName}! How old are you? ");
+//string? textAge = Console.ReadLine();
+//bool isValidAge = int.TryParse(textAge, out int age);
 
-if (!isValidAge)
-{
-    age = 0;
-}
+//if (!isValidAge)
+//{
+//    age = 0;
+//}
 
-decimal? walletBudget = 150.50M;
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-TimeOnly openingTime = TimeOnly.Parse("08:30");
-DateOnly dateOfBirth = DateOnly.ParseExact("15/06/1998", "dd/MM/yyyy", CultureInfo.InvariantCulture);
-Console.WriteLine($"Hello everybody, Check in starts at {openingTime} on {dateOfBirth}");
+//decimal? walletBudget = 150.50M;
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
-
-if (firstName.ToLower() == "tim" &&
-    lastName.ToLower() == "cory")
-{
-    Console.WriteLine("Hello Professor Corey, VIP Access Granted!.");
-}
-else if (firstName.ToLower() == "bob" ||
-         firstName.ToLower() == "sue")
-{
-    Console.WriteLine("Welcome back, Staff Member.");
-}
-else
-{
-    Console.WriteLine("Hello Student, welcome to the conference.");
-}
+//TimeOnly openingTime = TimeOnly.Parse("08:30");
+//DateOnly dateOfBirth = DateOnly.ParseExact("15/06/1998", "dd/MM/yyyy", CultureInfo.InvariantCulture);
+//Console.WriteLine($"Hello everybody, Check in starts at {openingTime} on {dateOfBirth}");
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-switch (age)
-{
-    case >= 0 and < 18:
-        Console.WriteLine($"You are a minor, you have {18 - age} years left. Please get a guardian's signature.");
-        break;
-    case >= 18 and < 65:
-        Console.WriteLine("Full access granted to all tech talks.");
-        break;
-    case > 65:
-        Console.WriteLine("Welcome! Ask about our senior developer lounge.");
-        break;
-    default:
-        Console.WriteLine("Welcome! Ask about our senior developer lounge.");
-        break;
-}
+
+
+//if (firstName.ToLower() == "tim" &&
+//    lastName.ToLower() == "cory")
+//{
+//    Console.WriteLine("Hello Professor Corey, VIP Access Granted!.");
+//}
+//else if (firstName.ToLower() == "bob" ||
+//         firstName.ToLower() == "sue")
+//{
+//    Console.WriteLine("Welcome back, Staff Member.");
+//}
+//else
+//{
+//    Console.WriteLine("Hello Student, welcome to the conference.");
+//}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-string? filePath = @"C:\Conference\Tickets\2026";
-Console.WriteLine($"My file path is {filePath}");
+//switch (age)
+//{
+//    case >= 0 and < 18:
+//        Console.WriteLine($"You are a minor, you have {18 - age} years left. Please get a guardian's signature.");
+//        break;
+//    case >= 18 and < 65:
+//        Console.WriteLine("Full access granted to all tech talks.");
+//        break;
+//    case > 65:
+//        Console.WriteLine("Welcome! Ask about our senior developer lounge.");
+//        break;
+//    default:
+//        Console.WriteLine("Welcome! Ask about our senior developer lounge.");
+//        break;
+//}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//string? filePath = @"C:\Conference\Tickets\2026";
+//Console.WriteLine($"My file path is {filePath}");
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
