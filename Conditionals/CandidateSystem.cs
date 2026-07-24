c336532c023bd574b2a6257280eb84194442f478
AnnaRuben <shaked12072014@gmail.com>
2026-07-24 08:20:28 +0300

    Candidate System

---
+++ Conditionals/CandidateSystem.cs
@@ -0,0 +1,124 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Conditionals
+//{
+//    internal class CandidateSystem
+//    {
+//    }
+//}
+using System.ComponentModel.Design;
+using System.Globalization;
+
+Console.Write("Hello Candidate! What is your first name? ");
+string? firstName = Console.ReadLine();
+
+Console.Write($"Hello {firstName}! What is your last name? ");
+string? lastName = Console.ReadLine();
+
+
+
+if (firstName.ToLower() == "tim" ||
+    firstName.ToLower() == "sue" ||
+    firstName.ToLower() == "bob")
+{
+    Console.WriteLine($"Hello dear candidate {firstName}");
+}
+else
+{
+    Console.WriteLine($"Hello Mr./Ms. {firstName}");
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+Console.Write($"{firstName}, Please write your birth date in the exact format: (dd/MM/yyyy) ");
+string? birthDateText = Console.ReadLine();
+DateOnly birthday = DateOnly.ParseExact(birthDateText, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+Console.Write($"{firstName}, Please insert your age in digits: ");
+string? ageText = Console.ReadLine();
+
+bool isValidage = int.TryParse(ageText, out int age);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+Console.WriteLine($"{firstName}, What is your salary expectations? ");
+string? salaryExpectationsText = Console.ReadLine();
+decimal salaryAmount = decimal.Parse(salaryExpectationsText);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+bool isHired = false;
+
+switch (age)
+{
+    case >= 0 and < 18:
+        Console.WriteLine($"{firstName}, you are a minor, please wait {18 - age} years.");
+        break;
+    case >= 18 and <= 67:
+        if (salaryAmount <= 30000.5M)
+        {
+            isHired = true;
+            Console.WriteLine($"Congrats {firstName}, You are hired");
+        }
+        else
+        { 
+            Console.WriteLine($"Thank you dear {firstName}, we decided to move forward with another candidate.");
+        }
+        break;
+    case > 67:
+        Console.WriteLine($"Dear {firstName}, we wish you a Happy retirement.");
+        break;
+    default:
+        Console.WriteLine("Undefined");
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Conditionals/EligibilityCheckSystem.cs
+++ Conditionals/EligibilityCheckSystem.cs
@@ -8,114 +8,148 @@
 //    {
 //    }
 //}
-using System.Globalization;
-
-Console.Write("Hello User! ,What is your first name: ");
-string? firstName = Console.ReadLine();
-
-Console.Write($"Hello {firstName}! ,What is your last name: ");
-string? lastName = Console.ReadLine();
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-string? filePath = @"C:\Students\Grants";
-Console.WriteLine($"Save file to {filePath} ,based on your name: {firstName} {lastName}.");
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-Console.Write($"{firstName} ,What's your age: ");
-string? ageText = Console.ReadLine();
-
-bool isValidInt = int.TryParse(ageText, out int age);
-
-Console.WriteLine($"This is valid: {isValidInt}. The number was {age}.");
 
 
-if (!isValidInt)
-{
-    age = 0;
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-DateOnly birthday = DateOnly.Parse("23/06/1983");
+//using System.Globalization;
 
-Console.WriteLine(birthday.ToString("MMMM, dd, yyyy", CultureInfo.InvariantCulture));
-Console.WriteLine($"Birthday full format: {birthday}");
+//Console.Write("Hello User! ,What is your first name: ");
+//string? firstName = Console.ReadLine();
 
-Console.Write("Enter your birthdate (dd/MM/yyyy): ");
-string? birthDateText = Console.ReadLine();
+//Console.Write($"Hello {firstName}! ,What is your last name: ");
+//string? lastName = Console.ReadLine();
 
-// המרה לפי הפורמט המבוקש עם DateTime.ParseExact
-DateTime birthday = DateTime.ParseExact(birthDateText!, "dd/MM/yyyy", CultureInfo.InvariantCulture);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-Console.Write($"Hey {firstName}, What is your grade point average: ");
-string? gradePointText = Console.ReadLine();
-double gradePointAverage = double.Parse(gradePointText!);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-Console.Write($"Hey {firstName}, What is your monthly income: ");
-string? incomeText = Console.ReadLine();
-decimal monthlyIncome = decimal.Parse(incomeText!);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-bool? hasMilitaryService = null;
-hasMilitaryService = true;
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-if (firstName.ToLower() == "bob" || 
-    firstName.ToLower() == "sue")
-{
-    Console.WriteLine($" Hello Professor {firstName}");
-}
-else
-{
-    Console.WriteLine("Hello Student");
-}
-
-if ((age >= 40 && age < 50) ||
-    (age >= 70 && age < 80))
-{
-    Console.WriteLine("You are in your 40's or 70's");
-}
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-switch (age)
-{
-    case >= 0 and <= 17:
-        Console.WriteLine($"You are a minor. You need to wait {18 - age} years.");
-        break;
-    case >= 18 and <= 65:
-        Console.WriteLine("You are a fit for checkup.");
-        break;
-    case > 65:
-        Console.WriteLine("Wish you a Happy Retirement.");
-        break;
-    default:
-        Console.WriteLine("Undefined.");
-        break;
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-bool isComplete = true;
-bool isEligible = false;
 
-if (gradePointAverage >= 85.5 &&
-    monthlyIncome < 6000M &&
-    isComplete)
-{
-    isEligible = true;
-    Console.WriteLine("Congrats! Registration process was completed successfully");
-}
-else
-{
-    Console.WriteLine("Sorry, try next year.");
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-TimeOnly scholarship = TimeOnly.FromDateTime(DateTime.Now);
 
-Console.WriteLine($"\n--- FINAL REPORT ---");
-Console.WriteLine($"Name: {firstName} {lastName}");
-Console.WriteLine($"File Path: {filePath}");
-Console.WriteLine($"Birthday: {birthday.ToString("MMMM dd, yyyy", CultureInfo.InvariantCulture)}");
-Console.WriteLine($"Check Time: {scholarship}");
-// הדפסה של הסטטוס הסופי
-Console.WriteLine($"Is Eligible for Scholarship? {isEligible}");
+
+//string? filePath = @"C:\Students\Grants";
+//Console.WriteLine($"Save file to {filePath} ,based on your name: {firstName} {lastName}.");
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+//Console.Write($"{firstName} ,What's your age: ");
+//string? ageText = Console.ReadLine();
+
+//bool isValidInt = int.TryParse(ageText, out int age);
+
+//Console.WriteLine($"This is valid: {isValidInt}. The number was {age}.");
+
+
+//if (!isValidInt)
+//{
+//    age = 0;
+//}
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+//Console.Write("Enter your birthdate (dd/MM/yyyy): ");
+//string? birthDateText = Console.ReadLine();
+
+//// המרה לפי הפורמט המבוקש עם DateTime.ParseExact
+//DateTime birthday = DateTime.ParseExact(birthDateText!, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+//Console.Write($"Hey {firstName}, What is your grade point average: ");
+//string? gradePointText = Console.ReadLine();
+//double.TryParse(gradePointText, out double gradePointAverage);
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+//Console.Write($"Hey {firstName}, What is your monthly income: ");
+//string? incomeText = Console.ReadLine();
+//decimal monthlyIncome = decimal.Parse(incomeText!);
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+//bool? hasMilitaryService = null;
+//hasMilitaryService = true;
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//if (firstName.ToLower() == "bob" ||
+//    firstName.ToLower() == "sue")
+//{
+//    Console.WriteLine($" Hello Professor {firstName}");
+//}
+//else
+//{
+//    Console.WriteLine("Hello Student");
+//}
+
+//if ((age >= 40 && age < 50) ||
+//    (age >= 70 && age < 80))
+//{
+//    Console.WriteLine("You are in your 40's or 70's");
+//}
+
+
+//switch (age)
+//{
+//    case >= 0 and <= 17:
+//        Console.WriteLine($"You are a minor. You need to wait {18 - age} years.");
+//        break;
+//    case >= 18 and <= 65:
+//        Console.WriteLine("You are a fit for checkup.");
+//        break;
+//    case > 65:
+//        Console.WriteLine("Wish you a Happy Retirement.");
+//        break;
+//    default:
+//        Console.WriteLine("Undefined.");
+//        break;
+//}
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+//bool isComplete = true;
+//bool isEligible = false;
+
+//if (gradePointAverage >= 85.5 &&
+//    monthlyIncome < 6000M &&
+//    isComplete)
+//{
+//    isEligible = true;
+//    Console.WriteLine("Congrats! Registration process was completed successfully");
+//}
+//else
+//{
+//    Console.WriteLine("Sorry, try next year.");
+//}
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+//TimeOnly scholarship = TimeOnly.FromDateTime(DateTime.Now);
+
+//Console.WriteLine($"\n--- FINAL REPORT ---");
+//Console.WriteLine($"Name: {firstName} {lastName}");
+//Console.WriteLine($"File Path: {filePath}");
+//Console.WriteLine($"Birthday: {birthday.ToString("MMMM dd, yyyy", CultureInfo.InvariantCulture)}");
+//Console.WriteLine($"Check Time: {scholarship}");
+//// הדפסה של הסטטוס הסופי
+//Console.WriteLine($"Is Eligible for Scholarship? {isEligible}");
