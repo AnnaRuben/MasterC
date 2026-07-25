e633770a5eb9cd79d35393edeeae96826ecc4fab
AnnaRuben <shaked12072014@gmail.com>
2026-07-25 13:30:20 +0300

    Practice Bool

--- BestPractice/OnboardingSystem.cs
+++ BestPractice/OnboardingSystem.cs
@@ -8,177 +8,71 @@
 //    {
 //    }
 //}
-using System.Globalization;
-//1.1
-bool isRaining = true;
-
-Console.WriteLine(isRaining);
-
-isRaining = !isRaining;
-
-Console.WriteLine(isRaining);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-//1.2
-double averageGrade = 87.5;
-int studentCount = 24;
-decimal classBudget = 1500.75M;
-
-Console.WriteLine($"{averageGrade}, {studentCount}, {classBudget}");
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-//1.3
-Console.WriteLine("Hello User! Please write 2 numbers.");
-string? numberOneText = Console.ReadLine();
-string? numberTwoText = Console.ReadLine();
-
-bool isNumberOneValid = int.TryParse(numberOneText, out int numberOne);
-bool isNumberTwoValid = int.TryParse(numberTwoText, out int numberTwo);
-
-Console.WriteLine($"This is valid: {isNumberOneValid}, the number was {numberOne} and this is valid: {isNumberTwoValid}, the number was {numberTwo}");
-
-if (isNumberOneValid && isNumberTwoValid)
-{
-    int sum = numberOne + numberTwo;
-    int difference = numberOne - numberTwo;
-    double average = (double)numberOne / numberTwo;
-
-    Console.WriteLine($"Sum: {sum}, Difference: {difference}, Average: {average}");
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-//1.4
-string? middleName = null;
-Console.WriteLine(middleName);
-
-middleName = "anna";
-Console.WriteLine(middleName);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-//1.5
-string? filePathOne = "C:\\Projects\\Demo\\file.txt";
-
-string? filePathTwo = @"C:\Projects\Demo\file.txt";
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-//2-2.1
-DateTime meetingTime = DateTime.Now;
-Console.WriteLine(meetingTime.ToString("dddd, MMMM, dd, yyyy, hh:mm tt", CultureInfo.InvariantCulture));
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-//2.2
-Console.Write("Hey User! Please inert your birthday in this exact format: (dd/MM/yyyy)");
-string? birthdayDateText = Console.ReadLine();
-DateOnly birthday = DateOnly.Parse(birthdayDateText);
-Console.WriteLine("Your age is 37.");
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-//2.3
-TimeOnly openingTime = TimeOnly.Parse(birthdayDateText);
-
-
-
-
-
-
-
-
-//Console.WriteLine(meetingTime.ToString("MMMM dd, yyyy hh:mm tt zzz", CultureInfo.InvariantCulture));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //using System.Globalization;
+////1.1
+//bool isRaining = true;
 
-//Console.Write("Hello, What is your first name? ");
-//string? firstName = Console.ReadLine();
+//Console.WriteLine(isRaining);
 
-//Console.Write($"Hello {firstName}, What is your last name? ");
-//string? lastNmae = Console.ReadLine();
+//isRaining = !isRaining;
 
-//Console.WriteLine($"Hello {firstName}, Please write your birthday date in this exact format: dd/mm/yy.: ");
+//Console.WriteLine(isRaining);
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+////1.2
+//double averageGrade = 87.5;
+//int studentCount = 24;
+//decimal classBudget = 1500.75M;
+
+//Console.WriteLine($"{averageGrade}, {studentCount}, {classBudget}");
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+////1.3
+//Console.WriteLine("Hello User! Please write 2 numbers.");
+//string? numberOneText = Console.ReadLine();
+//string? numberTwoText = Console.ReadLine();
+
+//bool isNumberOneValid = int.TryParse(numberOneText, out int numberOne);
+//bool isNumberTwoValid = int.TryParse(numberTwoText, out int numberTwo);
+
+//Console.WriteLine($"This is valid: {isNumberOneValid}, the number was {numberOne} and this is valid: {isNumberTwoValid}, the number was {numberTwo}");
+
+//if (isNumberOneValid && isNumberTwoValid)
+//{
+//    int sum = numberOne + numberTwo;
+//    int difference = numberOne - numberTwo;
+//    double average = (double)numberOne / numberTwo;
+
+//    Console.WriteLine($"Sum: {sum}, Difference: {difference}, Average: {average}");
+//}
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+////1.4
+//string? middleName = null;
+//Console.WriteLine(middleName);
+
+//middleName = "anna";
+//Console.WriteLine(middleName);
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+////1.5
+//string? filePathOne = "C:\\Projects\\Demo\\file.txt";
+
+//string? filePathTwo = @"C:\Projects\Demo\file.txt";
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+////2-2.1
+//DateTime meetingTime = DateTime.Now;
+//Console.WriteLine(meetingTime.ToString("dddd, MMMM, dd, yyyy, hh:mm tt", CultureInfo.InvariantCulture));
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+////2.2
+//Console.Write("Hey User! Please inert your birthday in this exact format: (dd/MM/yyyy)");
 //string? birthdayDateText = Console.ReadLine();
-////DateTime birthday = DateTime.ParseExact(birthdayDateText, "");
-////DateOnly birthday = DateOnly.ParseExact(birthdayDateText,"dd/")
-//DateOnly birthday = DateOnly.ParseExact(birthdayDateText, "dd/mm/yyyy", CultureInfo.InvariantCulture);
+//DateOnly birthday = DateOnly.Parse(birthdayDateText);
+//Console.WriteLine("Your age is 37.");
+////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+////2.3
+//TimeOnly openingTime = TimeOnly.Parse(birthdayDateText);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
---
+++ BestPractice/PracticeBool.cs
@@ -0,0 +1,131 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BestPractice
+//{
+//    internal class PracticeBool
+//    {
+//    }
+//}
+
+//1.1
+bool isLoggedIn = false;
+Console.WriteLine(isLoggedIn);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//1.2 - תוקן: bool רגיל בלי ערך התחלתי, בלי nullable
+bool hasPermission;
+Console.WriteLine(hasPermission); // שימי לב: זה לא יתקמפל! שגיאה: Use of unassigned local variable 'hasPermission'
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//1.3
+bool isRaining = true;
+bool isCold = true;
+Console.WriteLine(isRaining);
+Console.WriteLine(isCold);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//2-2.1
+bool isAcive = true;
+Console.WriteLine(isAcive);
+Console.WriteLine(!isAcive);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//2.2
+bool isFinished = false;
+Console.WriteLine(isFinished);
+
+isFinished = !isFinished;
+Console.WriteLine(isFinished);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//2.3 - תוקן: שתי הפיכות נפרדות ברצף, בלי &&
+bool isOnline = true;
+isOnline = !isOnline;
+isOnline = !isOnline;
+Console.WriteLine(isOnline);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//3-3.1
+bool canVote = true;
+
+if (canVote)
+{
+    Console.WriteLine("You can Vote!");
+}
+else
+{
+    Console.WriteLine("You can't Vote.");
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//3.2 - תוקן: הודעות ברורות יותר
+bool isMember = false;
+
+if (!isMember)
+{
+    Console.WriteLine("Sorry, you are not a member.");
+}
+else
+{
+    Console.WriteLine("Congrats! You are a member.");
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//3.3
+bool hasAccess = false;
+
+if (hasAccess)
+{
+    Console.WriteLine("Welcome to my App.");
+}
+else
+{
+    Console.WriteLine("Sorry ,you aren't authorized");
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//4.1
+bool isValid = true;
+
+//4.2
+isValid = !isValid;
+
+//4.3
+Console.WriteLine(isValid);
+
+//4.4 - תוקן: הודעות ברורות ותואמות למצב בפועל
+if (isValid)
+{
+    Console.WriteLine("isValid is true.");
+}
+else
+{
+    Console.WriteLine("isValid is false.");
+}
+
+//4.5
+bool imTheBestInBoolVariables = false;
+Console.WriteLine(!imTheBestInBoolVariables);
+Console.WriteLine(imTheBestInBoolVariables);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//5.1
+int? wholeNumber = null;
+wholeNumber = 11;
+bool biggerThanEleven = true;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//5.2
+bool isWeekend = true;
+
+if (!isWeekend)
+{
+    Console.WriteLine("Time to rest.");
+}
+else
+{
+    Console.WriteLine("Time to work.");
+}
+
+
